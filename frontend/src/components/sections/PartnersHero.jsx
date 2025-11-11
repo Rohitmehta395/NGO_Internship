6f15dfc550093905e78cf7e4cd220e458a92c008
aayushsaw950 <aayushgupta1916@gmail.com>
2025-11-11 20:39:00 +0530

    Our Partners Page added

---
+++ frontend/src/components/sections/PartnersHero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../styles/PartnersHero.css";
+
+const PartnersHero = () => {
+  return (
+    <section className="events-hero">
+      <div className="events-overlay">
+        <div className="events-context">
+        <h1>Our Partners</h1>
+        <p>
+          Home <span>&#187;</span> Partners
+        </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PartnersHero;
---
+++ frontend/src/pages/Partners.jsx
@@ -0,0 +1,12 @@
+import PartnersHero from "../components/sections/PartnersHero";
+import OurPartners from "../components/sections/OurPartner";
+
+const Partners = () => { 
+    return (
+        <>
+        <PartnersHero />        
+        <OurPartners />
+        </>
+    );
+}   
+export default Partners;
