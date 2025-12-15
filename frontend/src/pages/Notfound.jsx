54d506bfa94f1aa36009b29fd811c0ed714a6ca4
Rohit Mehta <rohitmehtaddn@gmail.com>
2025-12-16 00:30:32 +0530

    Fixed the 404 not found page not working on vercel deployment

--- frontend/src/pages/Notfound.jsx
+++ frontend/src/pages/Notfound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
-    <div className="-mt-[80px] pt-[80px] min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center px-4">
+    <div className="-mt-20 pt-20 min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center px-4">
       <h1 className="text-7xl font-bold text-orange-400 mb-4">404</h1>
 
       <h2 className="text-2xl font-semibold text-gray-800 mb-2">
